dbd87301d1a0695bfcfaecf67b710f966f0b40b7
YoucefPC <youcefaddou@gmail.com>
2025-02-27 14:34:11 +0100

    finalized the 2 steps of the project

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,8 @@
+const gameMode = document.querySelector('#gameMode')
 const gameContainer = document.querySelector('#gameContainer')
+const modeBtn = document.querySelector('#modeBtn')
+const pvpBtn = document.querySelector('#pvp')
+const cpuBtn = document.querySelector('#cpu')
 
 const grid = [
     ['', '', ''],
@@ -7,14 +11,28 @@ const grid = [
 ]
 let currentPlayer = 'X'
 let gameOver = false
+let count = 0
+let isCpu = false
+
+pvpBtn.addEventListener('click', () => {
+    isCpu = false
+    document.querySelector('#playerTurn').textContent = `C'est au tour du joueur ${currentPlayer}`
+    reset()
+})
+
+cpuBtn.addEventListener('click', () => {
+    isCpu = true
+    document.querySelector('#playerTurn').textContent = `C'est au tour du joueur ${currentPlayer}`
+    reset()
+})
 
 function displayGrid() {
     grid.forEach((row, rowIndex) => {
         row.forEach((cell, cellIndex) => {
             const cellContainer = document.createElement('div')
             cellContainer.classList.add('cell')
-            cellContainer.addEventListener('click', () => playerMove(rowIndex,cellIndex))
-        gameContainer.appendChild(cellContainer)
+            cellContainer.addEventListener('click', () => playerMove(rowIndex, cellIndex))
+            gameContainer.appendChild(cellContainer)
         })
     })
 }
@@ -28,6 +46,7 @@ function playerMove(rowIndex, cellIndex) {
     }
     else {
         grid[rowIndex][cellIndex] = currentPlayer
+        count++
         updateGrid()
         if (checkWinner()) {
             message.textContent = `Le joueur ${currentPlayer} a gagné !`
@@ -38,6 +57,9 @@ function playerMove(rowIndex, cellIndex) {
         } else { // chgmt de joueur, décla ternaire pour eviter les if else
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
             document.querySelector('#playerTurn').textContent = `C'est au tour du joueur ${currentPlayer}`
+            if (isCpu && currentPlayer === 'O') {
+                setTimeout(() => cpuMove(), 800)
+            }
         }
     }
 }
@@ -46,7 +68,7 @@ function playerMove(rowIndex, cellIndex) {
 function updateGrid() {
     const cells = document.querySelectorAll('.cell')
     cells.forEach((cell, index) => {
-        const rowIndex = Math.floor(index/3 )  // calc index de la ligne
+        const rowIndex = Math.floor(index / 3)  // calc index de la ligne
         const cellIndex = index % 3  // calc index de la cellule 
         cell.textContent = grid[rowIndex][cellIndex]
     })
@@ -55,39 +77,83 @@ function updateGrid() {
 function checkWinner() {
     //verif des lignes
     for (let i = 0; i < 3; i++) {
-        if (grid[i][0] && grid[i][0] === grid[i][1] && grid[i][1] == grid[i][2] ) {
+        if (grid[i][0] && grid[i][0] === grid[i][1] && grid[i][1] == grid[i][2]) {
             return true
         }
     }
     //verif des colonnes
     for (let i = 0; i < 3; i++) {
-        if (grid[0][i] && grid[0][i] === grid[1][i] && grid[1][i] == grid[2][i] ) {
+        if (grid[0][i] && grid[0][i] === grid[1][i] && grid[1][i] == grid[2][i]) {
             return true
         }
     }
     //verif des diago
-        if (grid[0][0] && grid[0][0] === grid[1][1] && grid[1][1] == grid[2][2] ) {
+    for (let i = 0; i < 3; i+=2) {
+        if (grid[0][0+i] && grid[0][0+i] === grid[1][1] && grid[1][1] === grid[2][2-i]) {
             return true
         }
-        if (grid[0][2] && grid[0][2] === grid[1][1] && grid[1][1] == grid[2][0] ) {
-            return true
-        }
-        return false
     }
+    return false
+}
 // verifier si egalité ou pas (si les cellules sont vides ou pas)
 function isDraw() {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j< 3; j++) {
-            if (grid[i][j] === '') {
-                return false
-            }
+
+    return count === 9
+    // for (let i = 0; i < 3; i++) {
+    //     for (let j = 0; j< 3; j++) {
+    //         if (grid[i][j] === '') {
+    //             return false
+    //         }
+    //     }
+    // }
+    // return true
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function cpuMove() {
+    if (gameOver) {
+        document.querySelector('#message').textContent = 'Le jeu est terminé !'
+    }
+    let rowIndex
+    let cellIndex
+    // tant qu'une cellule n'est pas vide 
+    while (true) {
+        rowIndex = random(0, 2)
+        cellIndex = random(0, 2)
+        if (grid[rowIndex][cellIndex] === '') {
+            break
         }
     }
-    return true
-}    
+    // mouvement du cpu quasi identique a la fonction playerMove
+    grid[rowIndex][cellIndex] = currentPlayer
+    count++
+    updateGrid()
+    if (checkWinner()) {
+        message.textContent = `Le joueur ${currentPlayer} a gagné !`
+        gameOver = true
+    } else if (isDraw()) {
+        message.textContent = `Match nul !`
+        gameOver = true
+    } else { // chgmt de joueur, décla ternaire pour eviter les if else
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
+        document.querySelector('#playerTurn').textContent = `C'est au tour de ${currentPlayer}`
 
+    }
+}
 
-
-// function cpuMove() {
-
-// }
+function reset() {
+    grid.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+            grid[rowIndex][cellIndex] = ""  // On vide la grille
+            currentPlayer = 'X'
+            gameOver = false
+            count = 0
+            updateGrid()
+            document.querySelector("#message").textContent = ""
+            document.querySelector("#playerTurn").textContent = `C'est au tour du joueur ${currentPlayer}`
+        })
+    })
+}
